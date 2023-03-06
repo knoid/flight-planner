699e4d7c76eed6d2d185005255b0a1bbd067d7a0
Ariel Barabas <810664+knoid@users.noreply.github.com>
2023-03-06 20:33:13 -0300

    set date field as date

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -6,7 +6,14 @@ export default function Metadata() {
   const { metadata } = useStore();
   return (
     <Hiddenable printHidden={!(metadata.callSign || metadata.date || metadata.title)}>
-      <MetadataField label="Date" name="date" printAlign="left" size="small" />
+      <MetadataField
+        inputProps={{ type: 'date' }}
+        InputLabelProps={{ shrink: true }}
+        label="Date"
+        name="date"
+        printAlign="left"
+        size="small"
+      />
       <MetadataField label="Title" name="title" printAlign="center" size="small" />
       <MetadataField label="Call Sign" name="callSign" printAlign="right" size="small" />
     </Hiddenable>
