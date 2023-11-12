85f075e33b232719f5c9766e06d11da04628fa6b
Ariel Barabas <810664+knoid@users.noreply.github.com>
2023-11-12 19:49:47 -0300

    save map zoom and center between navigations

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,3 +1,6 @@
+import { LatLng } from 'leaflet';
+import { useMapEvents } from 'react-leaflet';
+
 import { MapContainer } from '../../components/map';
 import { useStore } from '../../components/store';
 import MapMarkers from './components/MapMarkers';
@@ -5,13 +8,29 @@ import RemainingFuel from './components/RemainingFuel';
 import ToggleRemainingFuel from './components/ToggleRemainingFuel';
 import ZoomControls from './components/ZoomControls';
 
+let center = new LatLng(0, 0);
+let zoom = 2;
+
+function SaveLocation() {
+  const map = useMapEvents({
+    moveend() {
+      center = map.getCenter();
+    },
+    zoomlevelschange() {
+      zoom = map.getZoom();
+    },
+  });
+  return null;
+}
+
 export const Component = function MapPage() {
   const { showReminderOnMap, toggleReminderOnMap } = useStore();
   return (
     <>
-      <MapContainer center={[0, 0]} zoom={2} zoomControl={false}>
+      <MapContainer center={center} zoom={zoom} zoomControl={false}>
         <MapMarkers />
         {showReminderOnMap && <RemainingFuel />}
+        <SaveLocation />
         <ZoomControls />
       </MapContainer>
       <ToggleRemainingFuel selected={showReminderOnMap} onChange={toggleReminderOnMap} />
