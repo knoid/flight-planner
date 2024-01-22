c60f20eda7846483ec0ff90e1508ca5eb4714272
Ariel Barabas <810664+knoid@users.noreply.github.com>
2024-01-22 09:44:03 -0300

    added coords and totals to flight plan

--- src/pages/plan-next/components/WaypointDetails.tsx
+++ src/pages/plan-next/components/WaypointDetails.tsx
@@ -8,12 +8,14 @@ import {
   styled,
   Typography,
 } from '@mui/material';
+import { LatLng } from 'leaflet';
 import { Marker } from 'react-leaflet';
 
 import { AirportIcon, DivIcon, MapContainer, WaypointIcon } from '../../../components/map';
 import { Composite, FrequencyType } from '../../../components/openAIP';
 import { useAirport, useReportingPoint } from '../../../components/POIsContext';
 import { Leg } from '../../../components/store/constants';
+import { useI18nContext } from '../../../i18n/i18n-react';
 import Labelled from './Labelled';
 
 const Identifier = styled('span')({ fontFamily: 'monospace' });
@@ -23,10 +25,27 @@ interface WaypointDetailsProps extends Pick<AccordionProps, 'expanded' | 'onChan
   leg: Leg;
 }
 
+type CardinalDirections = 'W' | 'S' | 'E' | 'N';
+
+/**
+ * Converts decimal degrees to degrees minutes and seconds.
+ * https://stackoverflow.com/a/5786281
+ */
+function convertDDToDMS(D: number, lng: boolean) {
+  return {
+    dir: (D < 0 ? (lng ? 'W' : 'S') : lng ? 'E' : 'N') as CardinalDirections,
+    deg: 0 | (D < 0 ? (D = -D) : D),
+    min: 0 | (((D += 1e-9) % 1) * 60),
+    sec: (0 | (((D * 60) % 1) * 6000)) / 100,
+  };
+}
+
 export default function WaypointDetails({ expanded, index, leg, onChange }: WaypointDetailsProps) {
   const airport = useAirport(leg._id);
   const reportingPoint = useReportingPoint(leg._id);
   const poi = airport || reportingPoint;
+  const latLng = poi?.latLng() || new LatLng(0, 0);
+  const { LL } = useI18nContext();
 
   return (
     <Accordion expanded={expanded} onChange={onChange}>
@@ -64,7 +83,7 @@ export default function WaypointDetails({ expanded, index, leg, onChange }: Wayp
         <Box sx={(theme) => ({ flexGrow: 1, padding: theme.spacing(1, 2, 2) })}>
           {airport && (
             <>
-              <Typography>Radios</Typography>
+              <Typography variant="subtitle1">Radios</Typography>
               {airport?.frequencies
                 ?.sort((a, b) => FrequencyType[a.type].localeCompare(FrequencyType[b.type]))
                 .map(({ _id, name, remarks, type, value }) => (
@@ -72,12 +91,33 @@ export default function WaypointDetails({ expanded, index, leg, onChange }: Wayp
                     {value} {remarks}
                   </Labelled>
                 ))}
-              <Typography>Runways</Typography>
+              <Typography variant="subtitle1">Runways</Typography>
               {airport?.runways?.map((runway) => (
                 <Typography key={runway._id}>
                   {runway.designator} {Composite[runway.surface.mainComposite]}
                 </Typography>
               ))}
+            </>
+          )}
+          {poi && (
+            <>
+              <Typography variant="subtitle1">Coordinates</Typography>
+              <div>
+                <Typography variant="caption">GPS</Typography>{' '}
+                {[latLng.lat, latLng.lng].map((n) => n.toFixed(4)).join(', ')}
+              </div>
+              <div>
+                <Typography variant="caption">DMS</Typography>{' '}
+                {[latLng.lat, latLng.lng]
+                  .map((n, index) => {
+                    const { dir, deg, min, sec } = convertDDToDMS(n, index > 0);
+                    return (
+                      [deg + 'º', `${min}`.padStart(2, '0') + "'", sec + '"'].join('') +
+                      LL.cardinalDirections[dir]()
+                    );
+                  })
+                  .join(' ')}
+              </div>
             </>
           )}
         </Box>
